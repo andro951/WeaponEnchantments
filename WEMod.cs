7bf4cbde47fb73f543e6a5ea4074eb986c08000a
Jerry Rivas <kiroto50@gmail.com>
2022-07-26 00:47:32 -0400

    Added aligned soul group (Soul of light or night)

--- WEMod.cs
+++ WEMod.cs
@@ -377,6 +377,11 @@ namespace WeaponEnchantments
 				ItemID.WorkBench, ItemID.BambooWorkbench, ItemID.BlueDungeonWorkBench, ItemID.BoneWorkBench, ItemID.BorealWoodWorkBench, ItemID.CactusWorkBench, ItemID.CrystalWorkbench, ItemID.DynastyWorkBench, ItemID.EbonwoodWorkBench, ItemID.FleshWorkBench, ItemID.FrozenWorkBench, ItemID.GlassWorkBench, ItemID.GoldenWorkbench, ItemID.GothicWorkBench, ItemID.GraniteWorkBench, ItemID.GreenDungeonWorkBench, ItemID.HoneyWorkBench, ItemID.LesionWorkbench, ItemID.LihzahrdWorkBench, ItemID.LivingWoodWorkBench, ItemID.MarbleWorkBench, ItemID.MartianWorkBench, ItemID.MeteoriteWorkBench, ItemID.MushroomWorkBench, ItemID.NebulaWorkbench, ItemID.ObsidianWorkBench, ItemID.PalmWoodWorkBench, ItemID.PearlwoodWorkBench, ItemID.PinkDungeonWorkBench, ItemID.PumpkinWorkBench, ItemID.RichMahoganyWorkBench, ItemID.SandstoneWorkbench, ItemID.ShadewoodWorkBench, ItemID.SkywareWorkbench, ItemID.SlimeWorkBench, ItemID.SolarWorkbench, ItemID.SpiderWorkbench, ItemID.SpookyWorkBench, ItemID.StardustWorkbench, ItemID.SteampunkWorkBench, ItemID.VortexWorkbench
 			}) ;
 			RecipeGroup.RegisterGroup("WeaponEnchantments:Workbenches", group);
+			group = new RecipeGroup(() => "Any Aligned Soul", new int[]
+			{
+				ItemID.SoulofLight, ItemID.SoulofNight
+			});
+			RecipeGroup.RegisterGroup("WeaponEnchantments:AlignedSoul", group);
 		}
 		/*public override void PostSetupContent()
 		{
