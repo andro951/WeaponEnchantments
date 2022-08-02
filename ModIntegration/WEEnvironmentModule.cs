8880ee56a8bbcfb148728bd5f35f25bb27648688
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-02 08:20:39 -0400

    fixed formatting

--- ModIntegration/WEEnvironmentModule.cs
+++ ModIntegration/WEEnvironmentModule.cs
@@ -15,38 +15,27 @@ namespace WeaponEnchantments.ModIntegration
     [ExtendsFromMod(MagicStorageIntegration.magicStorageName)]
     public class WEEnvironmentModule : EnvironmentModule
     {
-        /*- IEnumerable<Item> GetAdditionalItems(EnvironmentSandbox sandbox)
-        - void ModifyCraftingZones(EnvironmentSandbox sandbox, ref CraftingInformation information)
-        - void OnConsumeItemForRecipe(EnvironmentSandbox sandbox, Item item)
-        眼鏡さん(Glasses-san) — 06/29/2022
-        the above three are available through an EnvironmentModule type that can be inherited from
-        and, as an example:*/
         public override string Name => "WeaponEnchantments_EnvironmentModule";
-		public override IEnumerable<Item> GetAdditionalItems(EnvironmentSandbox sandbox)
-        {
+		public override IEnumerable<Item> GetAdditionalItems(EnvironmentSandbox sandbox) {
             return Main.LocalPlayer.GetWEPlayer().enchantingTable.essenceItem;
         }
-		public override void ModifyCraftingZones(EnvironmentSandbox sandbox, ref CraftingInformation information)
-		{
+		public override void ModifyCraftingZones(EnvironmentSandbox sandbox, ref CraftingInformation information) {
             int highestTableTierUsed = Main.LocalPlayer.GetWEPlayer().highestTableTierUsed;
             int baseTableTier = ModContent.TileType<Tiles.WoodEnchantingTable>();
             int tableTier;
-            if (highestTableTierUsed == 0)
+            if (highestTableTierUsed == 0) {
                 tableTier = baseTableTier;
-            else
-            {
+	}
+            else {
                 tableTier = baseTableTier - 5 + highestTableTierUsed;
             }
             if (tableTier > -1)
                 information.adjTiles[tableTier] = true;
 		}
-		public override void OnConsumeItemForRecipe(EnvironmentSandbox sandbox, Item item, int stack)
-        {
-			if (item.ModItem != null && item.ModItem is EnchantmentEssence)
-            {
+		public override void OnConsumeItemForRecipe(EnvironmentSandbox sandbox, Item item, int stack) {
+			if (item.ModItem != null && item.ModItem is EnchantmentEssence) {
                 int type0 = ModContent.ItemType<EnchantmentEssenceBasic>();
                 Main.LocalPlayer.GetWEPlayer().enchantingTable.essenceItem[item.type - type0].stack -= stack;
-
             }//Will be done by magic storage next update
             
             WEMod.consumedItems.Add(item.Clone());
