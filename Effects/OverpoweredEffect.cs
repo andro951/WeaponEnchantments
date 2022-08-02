e7d1e573f39d1b162dd9acd39c7703ac9ba242e1
Jerry Rivas <kiroto50@gmail.com>
2022-08-02 00:13:07 -0400

    Removed unoverriden overpowered effect hooks

--- Effects/OverpoweredEffect.cs
+++ Effects/OverpoweredEffect.cs
@@ -8,14 +8,7 @@ using Terraria.ModLoader;
 
 namespace WeaponEnchantments.Items.Enchantments.EnchantmentEffects {
     public class OverpoweredEffect : EnchantmentEffect {
-
-        public override void OnAfterHit(NPC npc, WEPlayer player, Item item, ref int damage, ref float knockback, ref bool crit, Projectile projectile = null) {
-            throw new NotImplementedException();
-        }
-
-        public override void OnModifyHit(NPC npc, WEPlayer player, Item item, ref int damage, ref float knockback, ref bool crit, int hitDirection, Projectile projectile = null) {
-            throw new NotImplementedException();
-        }
+        public OverpoweredEffect(float enchantmentPower) : base(enchantmentPower) { }
 
         public override void PostUpdateMiscEffects(WEPlayer player) {
             player.Player.GetDamage(DamageClass.Generic) += enchantmentPower;
