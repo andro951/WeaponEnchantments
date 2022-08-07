c084fb4ee07d0f206496c6606e49f9108eebe929
andro951 <andro951@gmail.com>
2022-08-07 12:39:14 -0400

    Replaces StatModifer into base class instead

--- Items/Enchantments/EmptyTestEnchantment.cs
+++ Items/Enchantments/EmptyTestEnchantment.cs
@@ -10,7 +10,7 @@ namespace WeaponEnchantments.Items.Enchantments {
 	{
 		public override EnchantmentEffect[] Effects { get => new EnchantmentEffect[] { // Right now it's "recalculated" every time it's applied. Might want to make this lazy.
 			new DebuffEffect(BuffID.OnFire, new Time(5), 0.10f + 0.5f * EnchantmentTier),
-			new MaxMP(new StatModifier(1,1, 100))
+			new MaxMP(1,1, 100)
 		}; }
 
 		public override string Artist => "Kiroto";
