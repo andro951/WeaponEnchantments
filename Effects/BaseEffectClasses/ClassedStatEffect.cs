9a797ff42ce3b1f248870adb7ec64b8b36eadabb
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 23:54:52 -0400

    Create ClassedStatEffect.cs

---
+++ Effects/BaseEffectClasses/ClassedStatEffect.cs
@@ -0,0 +1,19 @@
+using Terraria.ModLoader;
+using WeaponEnchantments.Common;
+using WeaponEnchantments.Common.Utility;
+using static WeaponEnchantments.WEPlayer;
+
+namespace WeaponEnchantments.Effects {
+    public abstract class ClassedStatEffect : StatEffect {
+        protected StatEffect(EStatModifier sm) {
+            EStatModifier = sm;
+        }
+
+        protected StatEffect(float additive = 0f, float multiplicative = 1f, float flat = 0f, float @base = 0f, DamageClass dc = null) : base(additive, multiplicative, flat, @base) {
+            damageClass = dc != null ? dc : DamageClass.Generic;
+		    }
+
+        public DamageClass damageClass { get; set; }
+        public override string DisplayName { get; } = $"{damageClass.S()} {base.DisplayName}";
+    }
+}
