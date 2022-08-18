5ffae558d0f1aba5e6dea61a8f23bb8801bd9d5b
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 23:11:28 -0400

    DamageAfterDefenses tooltip

--- Effects/EnchantStats/DamageAfterDefenses.cs
+++ Effects/EnchantStats/DamageAfterDefenses.cs
@@ -15,11 +15,10 @@ namespace WeaponEnchantments.Effects
     {
         public DamageAfterDefenses(float additive = 0f, float multiplicative = 1f, float flat = 0f, float @base = 0f, DamageClass dc = null) : base(additive, multiplicative, flat, @base) {
             damageClass = dc != null ? dc : DamageClass.Generic;
-            DisplayName = $"{damageClass.DisplayName} Damage (Bonus is applied after defenses. Not visible in weapon tooltip)";
         }
 
         public DamageClass damageClass { get; set; }
-        public override string DisplayName { get; }
+        public override string DisplayName { get; } = $"{damageClass.S()} Damage (Bonus is applied after defenses. Not visible in weapon tooltip)";
         public override EnchantmentStat statType => EnchantmentStat.DamageAfterDefenses;
 
 		public void ModifyHitDamage(ref float damageMultiplier, Item item, NPC target, ref int damage, ref float knockback, ref bool crit, int hitDirection, Projectile projectile) {
