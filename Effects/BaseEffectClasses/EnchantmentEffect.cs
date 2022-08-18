8d1c243d1bbeace9213513908d6f7048860ed942
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 21:59:40 -0400

    Summary changes

--- Effects/BaseEffectClasses/EnchantmentEffect.cs
+++ Effects/BaseEffectClasses/EnchantmentEffect.cs
@@ -15,60 +15,43 @@ namespace WeaponEnchantments.Effects {
         //    return f > 0 ? '+' : '\0';
         //}
 
-        public EnchantmentEffect(float enchantmentPower = 1f) {
-            this.EnchantmentPower = enchantmentPower;
-        }
+        //public EnchantmentEffect(float enchantmentPower = 1f) {
+        //    this.EnchantmentPower = enchantmentPower;
+        //}
 
         /// <summary>
-        /// <para>
-        /// The damage class effectiveness of the enchantment.
-        /// </para>
-        /// <para>
-        /// It maps <see cref="DamageClass">damage classes</see> to efficiency percentages.
-        /// </para>
-        /// <para>
-        /// For example, if DamageClass.Melee is mapped to 0.8f, melee weapon effectiveness will be set to 80%.
-        /// </para>
-        /// <para>
-        /// This can be used on the enchantment implementation, but is not required.
-        /// </para>
+        /// The EnchantmentStrength modifier based on damage type of the weapon.<br/>
+        /// It maps <see cref="DamageClass">damage classes</see> to efficiency percentages.<br/>
+        /// For example, if DamageClass.Melee is mapped to 0.8f, EnchantmentStrength will be reduced to 80% on melee weapons.<br/>
         /// </summary>
         protected Dictionary<DamageClass, float> EnchantmentDamageEfficiency = new Dictionary<DamageClass, float>();
 
         /// <summary>
-        /// <para>
-        /// The power of the effect.
-        /// </para>
-        /// <para>
-        /// It represents how strong the effect should be in the given instance, usually set in the constructor.
-        /// </para>
-        /// <para>
-        /// For example, if LifeSteal is mapped to an EnchantmentPower of 1f, LifeSteal would heal 100% of the damage done (if used with an effective damage class).<br \>
-        /// Another example is if Defense is mapped to an enchantment power of 5.5f, the defense would increase by 5 (it is floored for integers)
-        /// </para>
-        /// <para>
-        /// This can be used on the enchantment implementation, but is not required.
-        /// </para>
+        /// The strength of the effect.<br/>
+        /// For example, if LifeSteal is mapped to an EnchantmentPower of 1f, LifeSteal would heal 100% of the damage done.<br \>
+        /// Another example is if Defense is mapped to an enchantment power of 5.5f, the defense would increase by 5.<br/>
+        /// Affected by EnchantmentamageEfficiency and EfficiencyMultiplier<br/>
         /// </summary>
         public virtual float EnchantmentStrength { get; protected set; }
         
         /// <summary>
-        /// How effective the enchantment is.
-        /// Affected by armor efficiency (Armor, Weapon, Accesory)
+        /// Used to modify the EnchantmentStrength<br/>
+        /// Affected by the item type the enchantment is applied on. (Weapon, Armor, Accesory)<br/>
         /// </summary>
         public virtual float EfficiencyMultiplier { get; protected set; } = 1f;
         
         public abstract string DisplayName { get; protected set; } = Name.AddSpaces();
         public virtual string Tooltip { get; protected set; } = DisplayName;
         public virtual Color TooltipColor { get; protected set; } = new Color(0xaa, 0xaa, 0xaa);
-
         public virtual bool showTooltip => true;
+	    
         public virtual float SelfStackingPenalty { get; protected set; } = 0f;
+	    
         public float GetClassEfficiency(DamageClass dc) {
             if (EnchantmentDamageEfficiency.ContainsKey(dc))
                 return EnchantmentDamageEfficiency[dc];
 
             return 1f;
-		}
+        }
     }
 }
