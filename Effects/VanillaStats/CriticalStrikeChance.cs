0be656ddb2020b4e968333abf844d2f007bdafcc
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 23:59:20 -0400

    ClassedStatEffect

--- Effects/VanillaStats/CriticalStrikeChance.cs
+++ Effects/VanillaStats/CriticalStrikeChance.cs
@@ -9,13 +9,11 @@ using WeaponEnchantments.Common.Utility;
 using static WeaponEnchantments.WEPlayer;
 
 namespace WeaponEnchantments.Effects {
-    public class CriticalStrikeChance : StatEffect, IClassedEffect, IVanillaStat {
-        public CriticalStrikeChance(float additive = 0f, float multiplicative = 1f, float flat = 0f, float @base = 0f, DamageClass dc = null) : base(additive, multiplicative, flat * 100f, @base * 100f) {
+    public class CriticalStrikeChance : ClassedStatEffect, IVanillaStat {
+        public CriticalStrikeChance(float additive = 0f, float multiplicative = 1f, float flat = 0f, float @base = 0f, DamageClass dc = null) : base(additive, multiplicative, flat * 100f, @base * 100f, dc) {
             damageClass = dc != null ? dc : DamageClass.Generic;
         }
 
-        public DamageClass damageClass { get; set; }
-        public override string DisplayName { get; } = $"{damageClass.S()} Critical Strike Chance";
         public override EnchantmentStat statType => EnchantmentStat.CriticalStrikeChance;
     }
 }
