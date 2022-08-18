c0e6b70e81cc750c4e96f794487d99daa997af85
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 23:56:15 -0400

    ClassedStatEffect

--- Effects/Interfaces/IClassedEffect.cs
+++ Effects/Interfaces/IClassedEffect.cs
@@ -1,4 +1,4 @@
-﻿using System;
+/*﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,3 +10,4 @@ namespace WeaponEnchantments.Effects {
         public DamageClass damageClass { get; protected set; }
     }
 }
+*/
