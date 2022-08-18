cbaeb812145b2972aeed0c6cdc5c4265a28ac9c9
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 23:26:55 -0400

    AttackSpeed DisplayName

--- Effects/VanillaStats/AttackSpeed.cs
+++ Effects/VanillaStats/AttackSpeed.cs
@@ -14,11 +14,10 @@ namespace WeaponEnchantments.Effects {
     public class AttackSpeed : StatEffect, IClassedEffect, IVanillaStat {
         public AttackSpeed(float additive = 0f, float multiplicative = 1f, float flat = 0f, float @base = 0f, DamageClass dc = null) : base(additive, multiplicative, flat, @base) {
             damageClass = dc ?? DamageClass.Generic;
-            DisplayName = $"{(damageClass.Type > DamageClass.Generic.Type ? damageClass.DisplayName : "")} Speed";
         }
         
         public DamageClass damageClass { get; set; }
-        public override string DisplayName { get; }
+        public override string DisplayName { get; } = $"{damageClass.S()}Attack Speed";
         public override EnchantmentStat statType => EnchantmentStat.AttackSpeed;
     }
 }
