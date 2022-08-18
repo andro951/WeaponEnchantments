dd9b3cd7724980f8360fca58ef23a00b14dafe10
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-17 22:57:02 -0400

    Create ID.cs

---
+++ Common/Utility/ID.cs
@@ -0,0 +1,21 @@
+namespace WeaponEnchantments.Common.Utility
+{
+    public enum DamageTypeSpecificID {
+		Default,
+		Generic,
+		Melee,
+		MeleeNoSpeed,
+		Ranged,
+		Magic,
+		Summon,
+		SummonMeleeSpeed,
+		MagicSummonHybrid,
+		Throwing
+	} //Located in DamageClassLoader.cs
+
+	public enum ArmorSlotSpecificID {
+		Head,
+		Body,
+		Legs
+	}
+}
