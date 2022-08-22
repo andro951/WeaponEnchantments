ba51a6225e04ebbe467501f0860822e1f55aebc7
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-22 01:07:00 -0400

    finished

--- Effects/BaseEffectClasses/PlayerSetEffect.cs
+++ Effects/BaseEffectClasses/PlayerSetEffect.cs
@@ -12,62 +12,25 @@ using WeaponEnchantments.Common.Utility;
 
 namespace WeaponEnchantments.Effects {
     public abstract class PlayerSetEffect : EnchantmentEffect {
-        public EnchantmentEffect () {
-            
+        protected PlayerSetEffect(float minimumStrength, DifficultyStrength strengthData, bool prevent) {
+            MinimumStrength = minimumStrength;
+            EnableStat = !prevent;
+            StrengthData = strengthData;
         }
 
-        /// <summary>
-        /// The EnchantmentStrength modifier based on damage type of the weapon.<br/>
-        /// It maps <see cref="DamageClass">damage classes</see> to efficiency percentages.<br/>
-        /// For example, if DamageClass.Melee is mapped to 0.8f, EnchantmentStrength will be reduced to 80% on melee weapons.<br/>
-        /// </summary>
-        protected Dictionary<DamageClass, float> EnchantmentDamageEfficiency = new Dictionary<DamageClass, float>();
+        public DifficultyStrength StrengthData;
+        public float MinimumStrength;
+		public abstract EnchantmentStat statName { get; }
 
-        protected DifficultyStrength EnchantmentStrengthData;
-        /// <summary>
-        /// The strength of the effect.<br/>
-        /// For example, if LifeSteal is mapped to an EnchantmentStrength of 1f, LifeSteal would heal 100% of the damage done.<br \>
-        /// Another example is if Defense is mapped to an enchantment power of 5.5f, the defense would increase by 5.<br/>
-        /// Affected by EnchantmentamageEfficiency and EfficiencyMultiplier<br/>
-        /// </summary>
-        public virtual float EffectStrength { get; protected set; }
-        
-        /// <summary>
-        /// Used to modify the EnchantmentStrength<br/>
-        /// Affected by the item type the enchantment is applied on. (Weapon, Armor, Accesory)<br/>
-        /// </summary>
-        public virtual float CombinedMultiplier {
-            get => combinedMultiplier;
-            protected set => combinedMultiplier = value;
-        }
-        protected float combinedMultiplier = 1f;
-        public virtual float AllowedListMultiplier {
-            get => allowedListMultiplier;
-            set {
-                allowedListMultiplier = value;
-                CombinedMultiplier = allowedListMultiplier * damageClassMultiplier;
+        public override string Tooltip {
+            get {
+                if (StrengthData != null && MinimumStrength > StrengthData.Value)
+                    return "";
+
+                return $"{DisplayName}{": " + (EnableStat ? "Enabled" : "Prevented")}";
             }
-        }
-        protected float allowedListMultiplier = 1f;
-        protected virtual float DamageClassMultiplier {
-            get => damageClassMultiplier;
-            set {
-                damageClassMultiplier = value;
-                CombinedMultiplier = allowedListMultiplier * damageClassMultiplier;
-            }
-        }
-        protected float damageClassMultiplier = 1f;
-
-
-        public virtual string DisplayName => GetType().Name.AddSpaces();
-        public virtual string Tooltip => DisplayName;
-        public virtual Color TooltipColor { get; protected set; } = Color.White;
-        public virtual bool showTooltip => true;
+	}
 	    
-        public virtual float SelfStackingPenalty { get; protected set; } = 0f;
-
-        public void SetDamageClassMultiplier(DamageClass dc) {
-            DamageClassMultiplier = EnchantmentDamageEfficiency.ContainsKey(dc) ? EnchantmentDamageEfficiency[dc] : 1f;
-        }
+	public abstract void SetEffect();
     }
 }
