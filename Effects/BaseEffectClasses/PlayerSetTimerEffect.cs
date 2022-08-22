52972fa0ca801fc4e0dd500c7d0acbf9a51e9b35
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-22 01:52:24 -0400

    Create PlayerSetTimerEffect.cs

---
+++ Effects/BaseEffectClasses/PlayerSetTimerEffect.cs
@@ -0,0 +1,48 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using WeaponEnchantments.Common;
+using WeaponEnchantments.Common.Utility;
+
+namespace WeaponEnchantments.Effects {
+    public abstract class PlayerSetTimerEffect : PlayerSetEffect, IUseTimer {
+        protected PlayerSetEffect(float minimumStrength, DifficultyStrength strengthData, bool prevent) {
+            MinimumStrength = minimumStrength;
+            EnableStat = !prevent;
+            StrengthData = strengthData;
+        }
+
+        public DifficultyStrength StrengthData;
+        public float MinimumStrength;
+		    public abstract EnchantmentStat statName { get; }
+        public virtual Time TimerDuration { set; get; };
+
+        public override string Tooltip {
+            get {
+                if (StrengthData != null && MinimumStrength > StrengthData.Value)
+                    return "";
+
+                return $"{(EnableStat ? "Grants" : "Prevents")} {DisplayName} ({TimerDuration} cooldown)"";
+            }
+	      }
+	    
+	      public abstract void SetEffect();
+        
+        public virtual void SetTimer(WEPlayer wePlayer) {
+          wePlayer.SetEffectTimer(this);
+        }
+        public virtual void CheckTimer() {
+          Main.LocalPlayer.GetWEPlayer().CheckTimer(this);
+        }
+        public virtual void TimerEnd() { }
+        public virtual void ActivateEffect(WEPlayer wePlayer) {
+          SetTimer(wePlayer);
+        }
+    }
+}
