5bc053f34aa361bd82124d8138136e21aeb80d6b
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-22 05:37:39 -0400

    SorlarDash

--- Items/Enchantments/Unique/SolarDashEnchantment.cs
+++ Items/Enchantments/Unique/SolarDashEnchantment.cs
@@ -3,14 +3,16 @@ using WeaponEnchantments.Common.Utility;
 
 namespace WeaponEnchantments.Items.Enchantments.Unique
 {
-	public abstract class PhaseJumpEnchantment : Enchantment
+	public abstract class SolarDashEnchantment : Enchantment
 	{
-		public override string CustomTooltip => $"(Dash)";
 		public override int StrengthGroup => 10;
 		public override float ScalePercent => 0.6f;
 		public override int ArmorSlotSpecific => (int)ArmorSlotSpecificID.Legs;
 		public override void GetMyStats() {
-			AddStaticStat("dashType", 0f, 1f, 0f, 3f);
+			Effects = new() {
+				
+			}
+			//AddStaticStat("dashType", 0f, 1f, 0f, 3f);
 
 			AllowedList = new Dictionary<EItemType, float>() {
 				{ EItemType.Armor, 1f }
@@ -20,9 +22,9 @@ namespace WeaponEnchantments.Items.Enchantments.Unique
 		public override string Artist => "andro951";
 		public override string Designer => "andro951";
 	}
-	/*public class PhaseJumpEnchantmentBasic : PhaseJumpEnchantment { }
-	public class PhaseJumpEnchantmentCommon : PhaseJumpEnchantment { }
-	public class PhaseJumpEnchantmentRare : PhaseJumpEnchantment { }
-	public class PhaseJumpEnchantmentSuperRare : PhaseJumpEnchantment { }
-	public class PhaseJumpEnchantmentUltraRare : PhaseJumpEnchantment { }*/
+	public class SolarDashEnchantmentBasic : SolarDashEnchantment { }
+	public class SolarDashEnchantmentCommon : SolarDashEnchantment { }
+	public class SolarDashEnchantmentRare : SolarDashEnchantment { }
+	public class SolarDashEnchantmentSuperRare : SolarDashEnchantment { }
+	public class SolarDashEnchantmentUltraRare : SolarDashEnchantment { }
 }
