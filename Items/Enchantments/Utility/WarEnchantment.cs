cb26f8601524cf342cfbec32e85ae2114ab41af6
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-30 04:28:00 -0400

    removed custom tooltip

--- Items/Enchantments/Utility/WarEnchantment.cs
+++ Items/Enchantments/Utility/WarEnchantment.cs
@@ -9,11 +9,6 @@ namespace WeaponEnchantments.Items.Enchantments.Utility
 {
 	public abstract class WarEnchantment : Enchantment
 	{
-		public override string CustomTooltip =>
-			"(Minion Damage is reduced by your spawn rate multiplier, from enchantments, unless they are your minion attack target)\n" +
-			"(minion attack target set from hitting enemies with whips or a weapon that is converted to summon damage from an enchantment)\n" +
-			"(Prevents consuming boss summoning items if spawn rate multiplier, from enchantments, is > 1.6)\n" +
-			"(Enemies spawned will be immune to lava/traps)";
 		public override int StrengthGroup => 2;
 		public override float ScalePercent => -1f;
 		public override SellCondition SellCondition => SellCondition.AnyTimeRare;
