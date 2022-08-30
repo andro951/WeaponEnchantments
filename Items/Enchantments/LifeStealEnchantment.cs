ab21e84e7da2acd2a51b06194a74af62d7b4b26e
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-30 04:07:08 -0400

    remove custom tooltip

--- Items/Enchantments/LifeStealEnchantment.cs
+++ Items/Enchantments/LifeStealEnchantment.cs
@@ -6,7 +6,6 @@ using WeaponEnchantments.Effects;
 
 namespace WeaponEnchantments.Items.Enchantments {
     public abstract class LifeStealEnchantment : Enchantment {
-        public override string CustomTooltip => $"(remainder is saved to prevent always rounding to 0 for low damage weapons)";
         public override float ScalePercent => 0.8f;
         public override bool Max1 => true;
         public override float CapacityCostMultiplier => 2f;
