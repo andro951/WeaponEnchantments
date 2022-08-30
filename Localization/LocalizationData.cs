215cb0474c8ad20ff50bf40518dffbfa4ef4bd0d
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-08-30 04:52:35 -0400

    added most enchantment effects

--- Localization/LocalizationData.cs
+++ Localization/LocalizationData.cs
@@ -39,11 +39,73 @@ namespace WeaponEnchantments.Localization
 			return returnValue;
 		}
 		public static bool ContainsText(string s, ) => s == Language.GetText(s).;
+		public static List<string> autoFill = new() {
+			"EnchantmentEffects",
+			"Debuffs"
+		}
 
 		public static SortedDictionary<string, SData> AllData => new() {
-			{ L_ID1.Tooltip.ToString(), new(children: new() { 
-				{ L_ID2.EnchantmentEffects.ToString(), new(dict: new() { 
-					{  }
+			{ L_ID1.Tooltip.ToString(), new(children: new() {
+				{ L_ID2.EffectDisplayName.ToString(), new(dict: new() {
+					{ "AmmoCost1", "Chance To Not Consume Ammo" },
+					{ "AmmoCost2", "Increased Ammo Cost" },
+					{ "DamageAfterDefenses", "Damage (Applied after defenses. Not visible in weapon tooltip)" },
+					{ "DamageClassChange", "\"Convert damage type to {0}\""}
+				}) }
+				{ L_ID2.EnchantmentEffects.ToString(), new(children: new() { 
+					{ "BoolEfect", new(dict: new() {
+						{ "Enabled", "\"{0} Enabled\"" },
+						{ "Enabled", "\"{0} Disabled\"" }
+					}) },
+					{ "BuffEffect", new(dict: new() {
+						{ BuffStyle.OnTickPlayerBuff.ToString(), "\"Passively grants {0} to you\"" },
+						{ BuffStyle.OnTickPlayerDebuff.ToString(), "\"Passively inflicts {0} to you\"" },
+						{ BuffStyle.OnTickAreaTeamBuff.ToString(), "\"Passively grants {0} to nearby players\"" },
+						{ BuffStyle.OnTickAreaTeamDebuff.ToString(), "\"Passively inflicts {0} to nearby players\"" },
+						{ BuffStyle.OnTickEnemyBuff.ToString(), "\"Passively grants {0} to enemy\"" },
+						{ BuffStyle.OnTickEnemyDebuff.ToString(), "\"Passively inflicts {0} to enemy\"" },
+						{ BuffStyle.OnTickAreaEnemyBuff.ToString(), "\"Passively grants {0} to nearby enemies\"" },
+						{ BuffStyle.OnTickAreaEnemyDebuff.ToString(), "\"Passively inflicts {0} to nearby enemies\"" },
+						{ BuffStyle.OnHitPlayerBuff.ToString(), "\"Grants you {0} on hit\"" },
+						{ BuffStyle.OnHitPlayerDebuff.ToString(), "\"Inflicts {0} to you on hit\"" },
+						{ BuffStyle.OnHitEnemyBuff.ToString(), "\"Grants {0} to enemies on hit\"" },
+						{ BuffStyle.OnHitEnemyDebuff.ToString(), "\"Inflicts {0} to enemies on hit\"" },
+						{ BuffStyle.OnHitAreaTeamBuff.ToString(), "\"Grants {0} to nearby players on hit\"" },
+						{ BuffStyle.OnHitAreaTeamDebuff.ToString(), "\"Inflicts {0} to nearby players on hit\"" },
+						{ BuffStyle.OnHitAreaEnemyBuff.ToString(), "\"Grants {0} to nearby enemies on hit\"" },
+						{ BuffStyle.OnHitAreaEnemyDebuff.ToString(), "\"Passively inflicts {0} to nearby enemies on hit\"" }
+					}) },
+					{ "CustomEffect", new(dict: new() {
+						{ "AllForOne", "\"(Item CD equal to {0}x use speed)\"" },
+						{ "AmmoCost", "\"{0} (Also Saves Bait When Fishing)\"" },
+						{ "EnemySpawnRate", 
+							"(Minion Damage is reduced by your spawn rate multiplier, from enchantments, unless they are your minion attack target)\n" +
+							"(minion attack target set from hitting enemies with whips or a weapon that is converted to summon damage from an enchantment)\n" +
+							"(Prevents consuming boss summoning items if spawn rate multiplier, from enchantments, is > 1.6)\n" +
+							"(Enemies spawned will be immune to lava/traps)"},
+						{ "FishingEnemySpawnChance", "\"{0} (Reduced by 5x during the day.  Affected by Chum Caster.  Can also spawn Duke Fishron)\"" },
+						{ "GodSlayer", "\"{0} (Bonus true damage based on enemy max hp)\n(Bonus damage not affected by LifeSteal)\"" },
+						{ "LavaFishing", "\"{0} (Allows fishing in lava and has a chance to improve catch rates in lava.  Stacks with other souces.)\""},
+						{ "MinionAttackTarget", "Enemies hit become the minion attack target.  Same effect as whips."},
+						{ "OnHitSpawnProjectile", "\"Spawns a projectile when hitting an enemy: {0}\""},
+						{ "QuestFishChance", "\"{0} (Quest fish caught will be automatically turned in and start a new quest, bypassing the 1 per day limmit.)\""}
+					}) },
+					{ "PlayerSetEffect", new(children: new() {
+						{ "VanillaDash", new(new() {
+							{ DashID.NinjaTabiDash.ToString() },
+							{ DashID.EyeOfCthulhuShieldDash.ToString() },
+							{ DashID.SolarDash.ToString() },
+							{ DashID.CrystalNinjaDash.ToString() }
+						}) }
+					})}
+				}) },
+				{ L_ID2.EnchantmentCustomTooltips.ToString(), new(dict: new() {
+					{ "WorldAblaze", 
+					 	"(Amaterasu debuff and below notes about it only apply at Enchantment tier 4.)\n" +
+						"(None shall survive the unstopable flames of Amaterasu)\n" +
+						"(Inflict a unique fire debuff to enemies that never stops)\n" +
+						"(The damage from the debuff grows over time and from dealing more damage to the target)\n" +
+						"(Spreads to nearby enemies and prevents enemies from being immune from other WorldAblaze debuffs.)" }
 				}) }
 			}) },
 			{ L_ID1.Dialogue.ToString(), new(children: new() {
@@ -82,9 +144,6 @@ namespace WeaponEnchantments.Localization
 					{ "Akko" },
 					{ "Kyubey" },
 					{ "Morgana" }
-				}) },
-				{ "NewNPC", new(new() {
-					{ "1" }
 				}) }
 			}) },
 			{ "Ores", new(new() { 
