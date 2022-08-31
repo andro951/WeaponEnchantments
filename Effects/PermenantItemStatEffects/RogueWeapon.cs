29ba8fb009f41dcab10f6210fa6c438f751d1b63
yoshisman8 <4214215+yoshisman8@users.noreply.github.com>
2022-08-30 20:42:37 -0400

    Added Calamitous and Eclipse enchantment

---
+++ Effects/PermenantItemStatEffects/RogueWeapon.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WeaponEnchantments.Common.Globals;
+using WeaponEnchantments.Common.Utility;
+
+namespace WeaponEnchantments.Effects
+{
+	public class RogueWeapon: EnchantmentEffect, IPermenantStat
+	{
+		public static DamageClassChange Default => new DamageClassChange(DamageClass.Generic);
+		public RogueWeapon() {}
+		public override string DisplayName => $"Convert damage type to Rogue. Requires Calamity mod to function.";
+
+		public DamageClass BaseDamageClass = null;
+
+		public void Update(ref Item item, bool reset = false) {
+			if (reset) {
+				Reset(ref item);
+			}
+			else {
+				ApplyTo(ref item);
+			}
+		}
+		public void ApplyTo(ref Item item) {
+			// We check for the Calamity mod first and foremost. 
+			// If the mod is not found, we execute .Reset()
+			if (ModLoader.HasMod("CalamityMod"))
+			{
+				Mod calamity = ModLoader.GetMod("CalamityMod");
+
+				// Pull "CalamityMod.RogueDamageClass" from the mod.
+				DamageClass rogueClass = calamity.Find<DamageClass>("RogueDamageClass");
+
+				if (rogueClass != null && item.TryGetEnchantedWeapon(out EnchantedWeapon enchantedWeapon))
+				{
+					item.DamageType = rogueClass;
+					if (BaseDamageClass == null)
+						BaseDamageClass = ContentSamples.ItemsByType[item.type].DamageType;
+
+					enchantedWeapon.damageType = rogueClass;
+					enchantedWeapon.baseDamageType = BaseDamageClass;
+				}
+                else
+                {
+					Reset(ref item);
+				}
+			}
+            else
+            {
+				Reset(ref item);
+            }
+			
+		}
+		public void Reset(ref Item item) {
+			// We must initialiez BaseDamageClass here too, otherwise we run into softlocks.
+			if (BaseDamageClass == null)
+				BaseDamageClass = ContentSamples.ItemsByType[item.type].DamageType;
+
+			item.DamageType = BaseDamageClass;
+			BaseDamageClass = null;
+				
+		}
+	}
+}
---
+++ Items/Enchantments/Unique/EclipseEnchantment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WeaponEnchantments.Common.Utility;
+using WeaponEnchantments.Effects;
+using static WeaponEnchantments.Common.EnchantingRarity;
+
+namespace WeaponEnchantments.Items.Enchantments.Unique
+{
+	public abstract class EclipseEnchantment : Enchantment
+	{
+		public override int StrengthGroup => 9;
+		public override float ScalePercent => 0.2f / defaultEnchantmentStrengths[StrengthGroup].enchantmentTierStrength[tierNames.Length - 1];
+		public override int RestrictedClass => (int)DamageTypeSpecificID.Magic;
+		public override void GetMyStats() {
+			Effects = new() {
+				new DamageAfterDefenses(multiplicative: EnchantmentStrengthData),
+				new DamageClassChange(DamageClass.Magic)
+			};
+
+			AllowedList = new Dictionary<EItemType, float>() {
+				{ EItemType.Weapons, 1f }
+			};
+		}
+
+		public override string Artist => "Vyklade";
+		public override string Designer => "Vyklade";
+	}
+	public class EclipseEnchantmentBasic : EclipseEnchantment
+	{
+		public override List<WeightedPair> NpcDropTypes => new() {
+			new(NPCID.Mothron)
+		};
+	}
+	public class EclipseEnchantmentCommon : EclipseEnchantment { }
+	public class EclipseEnchantmentRare : EclipseEnchantment { }
+	public class EclipseEnchantmentSuperRare : EclipseEnchantment { }
+	public class EclipseEnchantmentUltraRare : EclipseEnchantment { }
+}
