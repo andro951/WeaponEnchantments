1a47a6688afabfc95d87d68a0762a3d01be2c319
andro951 <andro951@gmail.com>
2022-09-02 12:07:30 -0400

    Moved KoKo lib to being a dependency instead of spliced into the mod so they can get more credit for their assistance.

--- WEMod.cs
+++ WEMod.cs
@@ -282,45 +282,5 @@ namespace WeaponEnchantments
 				}
 			});
 		}
-
-
-		#region KokoLib Mod
-
-		//KokoLib by Tabizzz
-		//https://github.com/Tabizzz/KokoLib
-
-		public static ModHandler[] ModHandlers;
-		internal static List<ModHandler> Handlers = new();
-
-		public override void PostSetupContent() {
-			foreach (var mod in ModLoader.Mods) {
-				TypeEmitter.EmittersForMod(mod);
-			}
-
-			Handlers.Sort((h, o) => string.Compare(h.Name, o.Name, StringComparison.Ordinal));
-
-			ModHandlers = Handlers.ToArray();
-			byte vid = 0;
-			foreach (var handler in ModHandlers) {
-				handler.Type = vid++;
-			}
-
-			foreach (var handle in Handlers) {
-				handle.CreateMethods();
-				handle.CreateProxy();
-			}
-
-			Handlers.Clear();
-			Handlers = null;
-		}
-		public override void HandlePacket(BinaryReader reader, int whoAmI) {
-			var index = reader.ReadByte();
-			var method = reader.ReadByte();
-			ModHandlers[index].WhoAmI = whoAmI;
-			ModHandlers[index].Handle(reader, method);
-			ModHandlers[index].WhoAmI = -1;
-		}
-
-		#endregion
 	}
 }
