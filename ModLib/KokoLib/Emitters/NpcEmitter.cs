31fcc799aeb00fa5752cea0ad2975bc33a02c9c5
andro951 <andro951@gmail.com>
2022-09-02 12:07:47 -0400

    Moved KoKo lib to being a dependency instead of spliced into the mod so they can get more credit for their assistance.

---
+++ ModLib/KokoLib/Emitters/NpcEmitter.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ID;
+
+namespace KokoLib.Emitters;
+
+class NpcEmitter : ModHandlerEmitter<NPC>
+{
+	public override NPC Read(BinaryReader reader)
+	{
+		var npc = Main.npc[reader.ReadInt32()];
+
+		if(Main.netMode == NetmodeID.Server)
+		{
+			npc.netUpdate = true;
+		}
+		return npc;
+	}
+
+	public override void Write(BinaryWriter writer, NPC ins)
+	{
+		writer.Write(ins.whoAmI);
+	}
+}
