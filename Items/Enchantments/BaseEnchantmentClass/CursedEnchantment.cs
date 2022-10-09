77188d8c83887777526eefefbed9b680c82e2a98
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-10-09 18:49:19 -0400

    added loads/net

--- Items/Enchantments/BaseEnchantmentClass/CursedEnchantment.cs
+++ Items/Enchantments/BaseEnchantmentClass/CursedEnchantment.cs
@@ -19,6 +19,73 @@ using WeaponEnchantments.Common.Globals;
 
 namespace WeaponEnchantments.Items {
 	public class CursedEnchantment : Enchantment {
-                
+                public override Load() {
+			
+		}
+		public override Save() {
+			
+		}
+		public override NetRecieve() {
+			
+		}
+		public override NetSend() {
+			
+		}
+		public override SetStaticDefaults() {
+			base.SetStaticDefaults();
+		}
+		public override SetDefaults() {
+			base.SetDefaults();
+			Item.maxStack = 1;
+		}
+		public override CanStack() => false;
+		
         }
 }
+
+/*
+public abstract class AllForOneEnchantment : Enchantment
+	{
+		public override int StrengthGroup => 6;
+		public override float ScalePercent => 0.8f;
+		public override bool Max1 => true;
+		public override List<int> RestrictedClass => new() { (int)DamageClassID.Summon };
+		public override void GetMyStats() {
+			Effects = new() {
+				new AllForOne(EnchantmentStrengthData * 0.4f + 4f),
+				new DamageAfterDefenses(multiplicative: EnchantmentStrengthData),
+				new NPCHitCooldown(multiplicative: EnchantmentStrengthData * 0.4f + 4f),
+				new AttackSpeed(multiplicative: (EnchantmentStrengthData * 0.1f + 1f).Invert()),
+				new ManaUsage(@base: EnchantmentStrengthData * 0.15f + 1.5f),
+				new AutoReuse(prevent: true)
+			};
+
+			AllowedList = new Dictionary<EItemType, float>() {
+				{ EItemType.Weapons, 1f }
+			};
+		}
+
+		public override string ShortTooltip => $"{Math.Round(EnchantmentStrength * AllowedListMultiplier, 3)}x {GetLocalizationTypeName()}";
+		public override string Artist => "Zorutan";
+		public override string ArtModifiedBy => null;
+		public override string Designer => "andro951";
+	}
+	public class AllForOneEnchantmentBasic : AllForOneEnchantment
+	{
+		public override SellCondition SellCondition => SellCondition.PostSkeletron;
+		public override List<WeightedPair> NpcDropTypes => new() {
+			new(NPCID.Mothron)
+		};
+		public override SortedDictionary<ChestID, float> ChestDrops => new() {
+			{ ChestID.Gold_Locked, 1f },
+			{ ChestID.Lihzahrd, 1f }
+		};
+		public override List<WeightedPair> CrateDrops => new() {
+			new(CrateID.Golden_LockBox, 0.45f)
+		};
+	}
+	public class AllForOneEnchantmentCommon : AllForOneEnchantment { }
+	public class AllForOneEnchantmentRare : AllForOneEnchantment { }
+	public class AllForOneEnchantmentEpic : AllForOneEnchantment { }
+	public class AllForOneEnchantmentLegendary : AllForOneEnchantment { }
+*/
