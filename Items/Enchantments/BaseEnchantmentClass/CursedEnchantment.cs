d6bb5c66370b22448f6ec0be9e80ba1070f23035
Isaac Richards <44043352+andro951@users.noreply.github.com>
2022-10-09 16:36:17 -0400

    usings

--- Items/Enchantments/BaseEnchantmentClass/CursedEnchantment.cs
+++ Items/Enchantments/BaseEnchantmentClass/CursedEnchantment.cs
@@ -1,4 +1,24 @@
-public class CursedEnchantment : Enchantment
-{
-  
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using WeaponEnchantments.Common;
+using WeaponEnchantments.Common.Configs;
+using System.Reflection;
+using Terraria.GameContent.Creative;
+using static WeaponEnchantments.Common.Configs.ConfigValues;
+using static WeaponEnchantments.Common.Utility.LogModSystem;
+using WeaponEnchantments.Common.Utility;
+using static WeaponEnchantments.Common.EnchantingRarity;
+using Terraria.Localization;
+using System.Linq;
+using WeaponEnchantments.Effects;
+using WeaponEnchantments.Common.Globals;
+
+namespace WeaponEnchantments.Items {
+	public class CursedEnchantment : Enchantment {
+                
+        }
 }
