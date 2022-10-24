eb92a60e134fc112e6d0a49d2e3df0123aa05c96
andro951 <andro951@gmail.com>
2022-10-24 15:53:32 -0400

    1.0.1.3(24OCT22)      -Changed description of config options from "received by" to "received from" for the essence and enchantment drop multipliers to make more sense.(@Zorutan)      -Corrected buff translations for "Buff Duration" in Italian(@Tefra_K), Russian(@4sent4) and Portuguese(@Dex?).      -Fixed Most Spanish translations being in Portuguese...... =_= (jose13032001 - Steam)

--- Common/Utility/LogSystem/LogModSystem.cs
+++ Common/Utility/LogSystem/LogModSystem.cs
@@ -31,7 +31,7 @@ namespace WeaponEnchantments.Common.Utility
         public static readonly bool printListForDocumentConversion = false;
         public static readonly bool zzzLocalizationForTesting = false;
         //public static bool printLocalizationKeysAndValues => printLocalizationKeysAndValues && culture == (int)CultureName.English;
-        public static readonly bool printLocalizationKeysAndValues = true && Debugger.IsAttached;
+        public static readonly bool printLocalizationKeysAndValues = false && Debugger.IsAttached;
         private static int localizationValuesCharacterCount = 0;
         public static bool printEnchantmentDrops => WEMod.clientConfig.PrintEnchantmentDrops;
         public static readonly bool printWiki = WEMod.clientConfig.PrintWikiInfo;
@@ -47,7 +47,6 @@ namespace WeaponEnchantments.Common.Utility
             { Armor, false },
             { Accessory, false }
         };
-
 
         //Only used to print the full list of contributors.
         public static Dictionary<string, string> contributorLinks = new Dictionary<string, string>() {
