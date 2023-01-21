5a2cc065b4c5719b542aaa0dbbb5ded147161e22
andro951 <andro951@gmail.com>
2023-01-21 08:00:27 -0500

    1.0.1.22(In progress)      -Changed Life Regen Enchantment is now a Utility based on vote results.

---
+++ Items/Enchantments/Utility/LifeRegenEnchantment.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Terraria.ID;
+using WeaponEnchantments.Common.Utility;
+using WeaponEnchantments.Effects;
+
+namespace WeaponEnchantments.Items.Enchantments.Utility
+{
+    public abstract class LifeRegenEnchantment : Enchantment
+    {
+        public override int StrengthGroup => 22;
+        public override void GetMyStats()
+        {
+            Effects = new() {
+                new LifeRegeneration(@base: EnchantmentStrengthData),
+            };
+
+            AllowedList = new Dictionary<EItemType, float>() {
+                { EItemType.Weapons, 0.5f },
+                { EItemType.Tools, 0.5f },
+                { EItemType.FishingPoles, 0.5f },
+                { EItemType.Armor, 1f },
+                { EItemType.Accessories, 1f }
+            };
+        }
+
+        public override string ShortTooltip => GetShortTooltip(sign: true, percent: false, multiply100: false);
+        public override string Artist => "Auseawesome";
+        public override string ArtModifiedBy => null;
+        public override string Designer => "Auseawesome";
+    }
+    public class LifeRegenEnchantmentBasic : LifeRegenEnchantment
+    {
+        public override SellCondition SellCondition => SellCondition.AnyTime;
+        public override List<WeightedPair> NpcDropTypes => new() {
+            new(NPCID.Zombie, 0.2f)
+        };
+        public override SortedDictionary<ChestID, float> ChestDrops => new() {
+            { ChestID.Chest_Normal, 0.5f }
+        };
+        public override List<WeightedPair> CrateDrops => new() {
+            new(CrateID.Wooden, 0.5f),
+            new(CrateID.Iron, 0.5f)
+        };
+    }
+    public class LifeRegenEnchantmentCommon : LifeRegenEnchantment { }
+    public class LifeRegenEnchantmentRare : LifeRegenEnchantment { }
+    public class LifeRegenEnchantmentEpic : LifeRegenEnchantment { }
+    public class LifeRegenEnchantmentLegendary : LifeRegenEnchantment { }
+}
