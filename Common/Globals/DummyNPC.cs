0b0a12919b64168fad649397a26cb67731b75ddf
Isaac Richards <44043352+andro951@users.noreply.github.com>
2023-02-08 02:10:15 -0500

    Added dps calculation to DummyNPC

--- Common/Globals/DummyNPC.cs
+++ Common/Globals/DummyNPC.cs
@@ -11,19 +11,56 @@ namespace WeaponEnchantments.Common.Globals
 	public class DummyNPC : GlobalNPC
 	{
 		private double dps => damage / (ticks / 60d);
-		private double damage;
 		private static int ticks = 300;
 		private int startingTick;
+		private static bool startDPSCheck;
+		public static bool StartDPSCheck {
+			get => startDPSCheck;
+			set {
+				startDPSCheck = value;
+				if (startDPSCheck)
+					startingTick = Main.GameUpdateCount;
+			}
+		}
+		private Dictionary<string, double> totalItemDamages = new();
 		public override bool AppliesToEntity(NPC entity, bool lateInstantiation) {
 			return entity.IsDummy();
 		}
 
 		public override void OnHitByItem(NPC npc, Player player, Item item, int damage, float knockback, bool crit) {
+			if (!WEMod.clientConfig.LogDummyDPS || !StartDPSCheck)
+				return;
 			
+			OnHitNPCWithAny(npc, item, damage, knockback, crit);
 		}
 		public override void OnHitByProjectile(NPC npc, Projectile projectile, int damage, float knockback, bool crit) {
+			if (!WEMod.clientConfig.LogDummyDPS || !StartDPSCheck)
+				return;
 			
+			projectile.TryGetGlobalProjectile(out WEProjectile weProj);
+            		Item item = weProj?.sourceItem;
+			OnHitNPCWithAny(npc, item, damage, knockback, crit, projectile);
 		}
-
+		private void OnHitNPCWith(NPC npc, Item item, float knockback, bool crit, Projectile projectile = null) {
+			//TODO: Check if damage includes crit or not for both melee and projectile
+			if (!totalItemDamages.ContainsKey(item.Name)) {
+				totalItemDamages.Add(item.Name, damage);
+			}
+			else {
+				totalItemDamages[item.Name] += damage;
+			}
+		}
+		private void CheckEndDPSCheck() {
+			//TODO: put thisinto an on tick method
+			if (Main.GameUpdateCount >= startingTick + ticks) {
+				StartDPSCheck = false;
+				foreach (KeyValuePair<string, double> pair in totalItemDamages) {
+					$"Dummy {NPC.whoAmI}, {pair.Key}, {pair.Value}".LogSimple();
+				}
+				
+				totalItemDamages.Empty();
+			}
+		}
+		//TODO: Add damage from loss of life
 	}
 }
