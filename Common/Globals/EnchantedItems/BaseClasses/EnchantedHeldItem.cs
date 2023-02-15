9b5b9863747264979b0726a1deab7792960fafa3
andro951 <andro951@gmail.com>
2023-02-15 15:42:07 -0500

    1.0.2.15(15FEB23)      -Thorium Techniques that deal damage now count as weapons.      -Fix for Executioner's Blade.  Reverted to the 1.0.2.3 update work around for this weapon only(@Yureii)      -Attempted to fix Weapon Enchantments not loading do to adding zero of an item to a recipe.(@Pixus)

--- Common/Globals/EnchantedItems/BaseClasses/EnchantedHeldItem.cs
+++ Common/Globals/EnchantedItems/BaseClasses/EnchantedHeldItem.cs
@@ -43,7 +43,18 @@ namespace WeaponEnchantments.Common.Globals
 			//Calamity Rogue weapon Attackspeed fix
 			if (WEMod.calamityEnabled && attackSpeed != 1f && !findingUseSpeed) {
 				if (ContentSamples.ItemsByType[item.type].DamageType == CalamityValues.rogue) {
-					player.GetAttackSpeed(CalamityValues.rogue) *= attackSpeed;
+					if (item.Name == "Executioner's Blade") {
+						findingUseSpeed = true;
+						float multiplier = CombinedHooks.TotalUseTimeMultiplier(player, item);
+						Item sampleItem = ContentSamples.ItemsByType[item.type];
+						item.useTime = Math.Max(1, (int)Math.Round(sampleItem.useTime * multiplier));
+						item.useAnimation = Math.Max(1, (int)Math.Round(sampleItem.useAnimation * multiplier));
+						findingUseSpeed = false;
+					}
+					else {
+						player.GetAttackSpeed(CalamityValues.rogue) *= attackSpeed;
+					}
+
 					return 1f;
 				}
 			}
