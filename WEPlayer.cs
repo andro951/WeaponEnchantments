c8530fa81fb516251d90734bab4d6c37f8dd476c
Auseawesome <ajthomasbtb@outlook.com>
2024-01-18 20:45:00 +0000

    Reverted everything I've messed up

--- WEPlayer.cs
+++ WEPlayer.cs
@@ -1620,7 +1620,7 @@ namespace WeaponEnchantments
                 case EnchantmentStat.MaxMinions:
                     Player.maxMinions = (int)sm.ApplyTo(Player.maxMinions);
                     break;
-                case EnchantmentStat.MaxMana:
+                case EnchantmentStat.MaxMP:
                     Player.statManaMax2 = (int)sm.ApplyTo(Player.statManaMax2);
                     break;
                 case EnchantmentStat.MaxFallSpeed:
